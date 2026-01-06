b790165636cd16eee23bb231fa1b186019e03485
thedhruvish <thedhruvish@gmail.com>
2026-01-06 11:01:55 +0530

    feat: Add Drizzle ORM with Neon database, Cloudflare deployment, update dependencies, and remove example component.

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     devtools(),
     nitro(),
     // this is the plugin that enables path aliases
